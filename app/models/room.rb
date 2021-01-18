f534e95a09ae40a1be745d832f194fc596362fa4
kuniterukbo <r2ka2h5e@icloud.com>
2021-01-18 19:03:54 +0900

    中間テーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :gender
+  has_many :room_users
+  has_many :users, through: :room_users
+  
   validates :gender_id, numericality: { other_than: 1 } 
 end
