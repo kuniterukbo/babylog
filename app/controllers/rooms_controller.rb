b6f21b7f17b532c46700310964fd12352fddb7a4
kuniterukbo <r2ka2h5e@icloud.com>
2021-01-19 00:02:24 +0900

    ルーム保存処理の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,18 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def room_params
+    params.require(:room).permit(:babyname, :gender_id, :birthday, user_ids: [])
+  end
+
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,8 @@
 class Room < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :gender
   has_many :room_users
   has_many :users, through: :room_users
-  
+
   validates :gender_id, numericality: { other_than: 1 } 
 end
