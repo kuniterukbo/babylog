83e92d45f660df4f25c914ad2ca3f9bbe7f3a20a
kuniterukbo <r2ka2h5e@icloud.com>
2021-01-25 21:43:31 +0900

    ルーム削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new, :create] do
+  resources :rooms, only: [:index, :new, :create,:destroy] do
     resources :posts, only: [:index, :new, :create, :show, :destroy] do
       resources :comments, only: [:create]
     end
