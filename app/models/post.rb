21602903c361469543c70b2a0afc67d3cba2c319
kuniterukbo <r2ka2h5e@icloud.com>
2021-01-31 11:16:52 +0900

    REEDME編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :event
   belongs_to :room
+  belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :posts
   has_many :comments
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
