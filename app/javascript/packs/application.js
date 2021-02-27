e243b0f51a9625bf6b7e68efffaf0fa464bbbb67
kuniterukbo <r2ka2h5e@icloud.com>
2021-02-28 08:38:16 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,5 @@ require("../post_search")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require jquery_ujs
--- app/javascript/post_search.js
+++ app/javascript/post_search.js
@@ -1,17 +1,8 @@
 function post() {
-  const submit = document.getElementById("submit");
-  const search_shooting_date = document.getElementById("shooting_date");
-
-  search_shooting_date.addEventListener("change", () => {
-    console.log(search_shooting_date.value);
-   const post_image = document.querySelectorAll(".post-image")
-   console.log(post_image)
-    // image.classList.add('none');
-    // const post_shooting_date = document.getElementsByClassName(search_shooting_date.value);
-    // console.log(post_shooting_date);
-    // post_shooting_date.classList.add('none');
-    // const oya = post_shooting_date.parentNode.parentNode;
-    // oya.style.display = 'none';
-  });
+  const $shooting_date = $("shooting_date")
+  const $test = $("#test");
+  console.log($test.value);
+  $test.text("bbb");
 }
-window.addEventListener("DOMContentLoaded", post)
+
+window.addEventListener("DOMContentLoaded", post);
