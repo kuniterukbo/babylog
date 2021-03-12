ddd6faa7b0ca1db33bc8daa1d62ba6b0d1635ea8
kuniterukbo <r2ka2h5e@icloud.com>
2021-03-12 10:45:41 +0900

    リファクタリング

--- app/javascript/post_search.js
+++ app/javascript/post_search.js
@@ -2,13 +2,13 @@ window.addEventListener("turbolinks:load", () =>{
   const $shooting_date = $("#shooting_date");
   const $event = $("#event_id");
   const $all_show_btn = $("#all_show_btn");
+  const $post_images = $(".post-images");
 
   let btn_display = false;
   show_change()
 
   // 日付で検索---------------------------
   $shooting_date.on("change",function() {
-    const $post_images = $(".post-images");
     $event.val(1);
     $post_images.hide();
     $(`.${$shooting_date.val()}`).show();
@@ -18,26 +18,26 @@ window.addEventListener("turbolinks:load", () =>{
   // --------------------------------------
   // イベントで検索----------------------
   $event.on("change",function(){
-    const $post_images = $(".post-images");
     $shooting_date.val("");
     $post_images.hide();
     $(`.${$event.val()}`).show();
     btn_display = true;
-    show_change()
+    show_change();
   });
   // -------------------------------------
 
+  //全て画像を表示-----------------------
   $all_show_btn.on("click", function(){
     $all_show_btn.hide();
-    $(".post-images").show();
+    $post_images.show();
   });
+//  -------------------------------------
+
 
   function show_change(){
     if(btn_display == true){
-      console.log(btn_display);
       $all_show_btn.show();
     } else {
-      console.log(btn_display);
       $all_show_btn.hide();
     }
   }
